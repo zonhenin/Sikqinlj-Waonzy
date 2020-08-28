8d587fcea4324bfe76cdd4261f5c0ab309fee68e
Nelson Lee <nelsonleezonhat@gmail.com>
2020-08-28 15:37:47 +0930

    Enhance web font

--- web.js
+++ web.js
@@ -1,15 +1,34 @@
-$(function() {
-	$('#babel-stone').click(function() {
-		$('.zhrr-st').each(function() {
-			$(this).text($(this).data('text'));
-		});
-		$('body').css('font-family', 'BabelStone Han');
+let defaultFonts='Helvetica, Tahoma, Arial, STXihei, "华文细黑", "Microsoft YaHei", "微软雅黑", sans-serif';
+
+function setPreferableFont() {
+	$('.zhrr-st').each(function() {
+		$(this).text($(this).data('text'));
 	});
 	
-	$('#safe-font').click(function() {
-		$('.zhrr-st').each(function() {
-			$(this).text($(this).data('alt'));
+	let oh=$('.zhrr-text').height()
+	$('body').css('font-family', '"BabelStone Han", '+defaultFonts);
+	let nh=$('.zhrr-text').height()
+	if (oh==nh) {
+		setCompatibleFont();
+		toastr["error"]('载入专用字体失败。\n为正确显示所有专用吴语汉字，请<button type="button" id="download-font-button">下载专用字体</button>，並刷新页面。');
+		$('#download-font-button').click(function() {
+			window.location.href = 'BabelStoneHan.ttf';
 		});
-		$('body').css('font-family', 'PingFang SC');
-	});
+	}
+}
+
+function setCompatibleFont() {	
+	$('.zhrr-st').each(function() {
+		$(this).text($(this).data('alt'));
+	});	
+	$('body').css('font-family', defaultFonts);
+}
+
+$(function() {
+	$('#babel-stone').click(setPreferableFont);	
+	$('#safe-font').click(setCompatibleFont);
+	
+	setCompatibleFont();
+	$('body').css('font-family', '');
+	setPreferableFont();
 });
